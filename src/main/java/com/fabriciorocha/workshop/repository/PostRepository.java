ce85b2b5a7055860aa70eae32ebe3db580d556ab
unknown <fabriciorochapalmeira@gmail.com>
2022-03-02 17:48:53 -0300

    Consulta simples com query methods

--- src/main/java/com/fabriciorocha/workshop/repository/PostRepository.java
+++ src/main/java/com/fabriciorocha/workshop/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.fabriciorocha.workshop.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.fabriciorocha.workshop.entities.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String title);
 }
--- src/main/java/com/fabriciorocha/workshop/resources/PostResource.java
+++ src/main/java/com/fabriciorocha/workshop/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.fabriciorocha.workshop.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fabriciorocha.workshop.entities.Post;
+import com.fabriciorocha.workshop.resources.util.URL;
 import com.fabriciorocha.workshop.services.PostService;
 
 @RestController
@@ -23,6 +27,14 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "title", defaultValue = "") String title){
+		
+		title = URL.decodeParam(title);
+		List<Post> posts = service.findByTitle(title);
+		return ResponseEntity.ok().body(posts);
 	}
 	
 	
