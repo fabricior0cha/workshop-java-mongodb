eee40b6ad9ef715f770a01f87a25117c70557f89
unknown <fabriciorochapalmeira@gmail.com>
2022-03-02 17:54:30 -0300

    Consulta simples com @Query

--- src/main/java/com/fabriciorocha/workshop/repository/PostRepository.java
+++ src/main/java/com/fabriciorocha/workshop/repository/PostRepository.java
@@ -3,10 +3,14 @@ package com.fabriciorocha.workshop.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.fabriciorocha.workshop.entities.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
 	List<Post> findByTitleContainingIgnoreCase(String title);
+	
+	@Query("{ title: { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String title);
 }
