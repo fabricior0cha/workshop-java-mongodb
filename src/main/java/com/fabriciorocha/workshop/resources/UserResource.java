f26e4cbbd9f04caf8a2ef41557663d63d33fd673
unknown <fabriciorochapalmeira@gmail.com>
2022-03-02 11:07:18 -0300

    Atualização de usuário com PUT

--- src/main/java/com/fabriciorocha/workshop/resources/UserResource.java
+++ src/main/java/com/fabriciorocha/workshop/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,4 +54,13 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> put(@PathVariable String id ,@RequestBody UserDTO userDto){
+		User user = service.fromDto(userDto);
+		user.setId(id);
+		user = service.update(user);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/fabriciorocha/workshop/services/UserService.java
+++ src/main/java/com/fabriciorocha/workshop/services/UserService.java
@@ -35,6 +35,19 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		return repository.save(newUser);
+		
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+		
+	}
+
 	public User fromDto(UserDTO dto) {
 		return new User(dto.getId(), dto.getName(), dto.getEmail());
 	}
